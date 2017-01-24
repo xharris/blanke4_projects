a93e1c6603ea2a8e6146cca40346ddb99ac38afb
Xavier Harris <xharris1@umbc.edu>
2017-01-24 05:19:12 -0500

    player animation

--- boredom/assets/scripts/entity/Player_22a02874.lua
+++ boredom/assets/scripts/entity/Player_22a02874.lua
@@ -23,36 +23,63 @@ function Player:init(x, y)
     self.img_walk = assets:img_player_walk()
    	self.spr_walk = assets:spr_player_walk()
     
-    self.anim = anim8.newAnimation(self.spr_walk('1-2',1), 0.1)
+    self.anim_walk = anim8.newAnimation(self.spr_walk('1-2',1), 0.1)
+
+    self.anim_state = "walk"
     
-    self.debug = true
+    self.xscale = 1
+    
+    self.debug = false
 end
 
 function Player:update(dt)
-    self.anim:update(dt)
+    -- animation
+    if love.keyboard.isDown("left") or love.keyboard.isDown("right") then
+	    -- walk on ground
+    	if self.can_jump then
+           self.anim_state = "walk" 
+        -- falling
+        else 
+            self.anim_state = "fall"
+        end
+    else
+    	-- falling
+        if math.abs(self.dy) ~= 0 then
+        	self.anim_state = "fall"
+        else
+            self.anim_state = "stand"
+        end
+    end
+    
+    if self.anim_state == "walk" then
+	    self.anim_walk:update(dt)
+    end
+    
         
     local body_collisions = HC.collisions(self.body)
     local feet_collisions = HC.collisions(self.feet)
-
-    -- left/right movement
-    if love.keyboard.isDown("left") then
-        self.body:move(-self.dx, 0)
-    end
-    if love.keyboard.isDown("right") then
-        self.body:move(self.dx, 0)
-    end
 
     -- gravity    
     self.dy = self.dy + (self.gravity * dt)
     self.gravity = self.GRAVITY 
 
+    -- left/right movement    
+    if love.keyboard.isDown("left") then
+        self.xscale = -1
+        self.body:move(-self.dx, 0)
+    end
+    if love.keyboard.isDown("right") then
+        self.xscale = 1
+        self.body:move(self.dx, 0)
+    end
+    
     -- body collisions
     for other, seperating_vector in pairs(body_collisions) do
         if other.type == "ground" then
             self.body:move(seperating_vector.x, seperating_vector.y)
         end        
     end 
-    
+        
     -- head collisions
     if math.abs(self.dy) > 0 then
     	local head_collisions = HC.collisions(self.head)
@@ -76,7 +103,6 @@ function Player:update(dt)
     
     -- jumping
     if love.keyboard.isDown("up") and self.can_jump then
-        
         self.dy = -self.jump_power
         self.can_jump = false
     end
@@ -92,17 +118,33 @@ function Player:update(dt)
 end
 
 function Player:draw()
-	self.anim:draw(self.img_walk, self.x, self.y)
+    -- flip offset
+    local flip_offx = 0
+    if self.xscale == -1 then
+       	flip_offx = 16
+    end
+    
+    -- draw animation
+    if self.anim_state == "walk" then
+        self.anim_walk:resume()
+		self.anim_walk:draw(self.img_walk, self.x, self.y, 0, self.xscale, 1, flip_offx)
+    elseif self.anim_state == "fall" then
+        self.anim_walk:pauseAtEnd()
+		self.anim_walk:draw(self.img_walk, self.x, self.y, 0, self.xscale, 1, flip_offx)
+    elseif self.anim_state == "stand" then
+        love.graphics.draw(self.img_stand, self.x, self.y, 0, self.xscale, 1, flip_offx)
+    end
+        
     
     if self.debug then
         love.graphics.setColor(255, 0, 0)
+        love.graphics.print(self.anim_state, 20, 20)
         self.body:draw('line')
         love.graphics.setColor(0, 255, 0)
         if math.abs(self.dy) > 0 then
         	self.head:draw('line')
         end
         self.feet:draw('line')
-        love.graphics.print(tostring(self.can_jump) .. ' ' .. tostring(vx) .. ' ' .. tostring(vy), 5, 5)
         love.graphics.setColor(255, 255, 255)
     end
 end
